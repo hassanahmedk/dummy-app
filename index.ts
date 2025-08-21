8641c33026d29f13817281af6d1c1a7bb1b05f09
Hassan <89640679+hassanahmedk@users.noreply.github.com>
2025-08-21 14:19:35 +0500

    feat: eas configurations

--- index.ts
+++ index.ts
@@ -2,3 +2,6 @@ import { registerRootComponent } from "expo";
 import App from "./App";
 
 registerRootComponent(App);
+// export PATH="/usr/local/opt/openjdk@17/bin:$PATH" && export JAVA_HOME="/usr/local/opt/openjdk@17" && eas build --platform android --local
+
+// installed jdk and android-commandlinetools for local builds
